d9a1119fc50f550241ae5c7fc35c5e567dd97d70
dhenriques399 <diegohenriques399@gmail.com>
2024-10-09 16:38:24 -0300

    09/10

---
+++ sistema de cadastro/server.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "produtos";
+
+
+//Cria a conexão
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+//Verifica a conexão
+if($conn -> connect_error){
+    die("Conexão falhou:".$conn->connect_error);
+}
+
+//Verifica ação e executa conforme necessário
+
+
+?>
