ae20df92cd72c66f14f2f7b0965e1e119b43238a
dhenriques399 <diegohenriques399@gmail.com>
2024-10-22 16:30:40 -0300

    22/10/2024

---
+++ CRUD_VALIDA/script.js
@@ -0,0 +1,19 @@
+document.querySelector('form').addEventListener('submit', function(event) { 
+  // Adiciona um evento de envio ao formulário
+  event.preventDefault(); // Previne o envio do formulário
+  
+  // Obtem os valores dos campos do formulário
+  var email = document.getElementById('email').value;
+  var senha = document.getElementById('senha').value;
+  var nome = document.getElementById('nome').value;
+  var telefone = document.getElementById('telefone').value;
+
+  
+  // Verifica se o email e a senha correspondem aos dados armazenados
+  if (nome === '' || telefone === '' || email === '' || senha === '') {
+    alert('Por favor,preencha todos os campos!');// Exibe uma mensagem de alerta
+    event.preventDefault();// Previne o envio do formulário 
+  } else {
+    form.reset();// Limpa o formulário
+  }
+});
